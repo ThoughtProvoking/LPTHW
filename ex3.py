f3174de8df6fe6a84fd776699d3008459d32bdd2
ThoughtProvoking <doreentlu0@gmail.com>
2017-06-03 11:56:19 -0400

    Finish Exercise 3: Numbers and Math

---
+++ ex3.py
@@ -0,0 +1,44 @@
+# Rewrote to use floating point numbers
+
+# Prints a line
+print('I will now count my chickens:')
+
+# Prints the number of hens (30 divide by 6, then add 25)
+print('Hens', 25.0 + 30.0 / 6.0)
+# Prints the number of roosters (25 multiply by 3, then mod 4, then subtract from 100)
+print('Roosters', 100.0 - 25.0 * 3.0 % 4.0)
+
+# Prints a line
+print('Now I will count the eggs:')
+
+# 4 mod 2, then 1 divide by 4, then add/subtract everything together and prints the result
+print(3.0 + 2.0 + 1.0 - 5.0 + 4.0 % 2.0 - 1.0 / 4.0 + 6.0)
+
+# Prints a line
+print('Is it true that 3 + 2 < 5 - 7?')
+
+# 3 add 2, then 5 subtract 7, then prints if sum is less than difference
+print(3.0 + 2.0 < 5.0 - 7.0)
+
+# Prints the result of 3 add 2
+print('What is 3 + 2?', 3.0 + 2.0)
+# Prints the result of 5 subtract 7
+print('What is 5 - 7?', 5.0 - 7.0)
+
+# Prints a line
+print("Oh, that's why it's False.")
+
+# Prints a line
+print('How about some more.')
+
+# Prints if 5 greater than -2
+print('Is it greater?', 5.0 > -2.0)
+# Prints if 5 greater than or equal to -2
+print('Is it greater or equal?', 5.0 >= -2.0)
+# Prints if 5 less than or equal -2
+print('Is it less or equal?', 5.0 <= -2.0)
+
+
+# My own calculation
+# Print average number of pages for given number of words
+print(743289.0 / 3000.0) # average number of words/page: 3000
