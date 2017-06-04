df503ec558e975024f2e4c1a3f05702a45a766f3
ThoughtProvoking <doreentlu0@gmail.com>
2017-06-04 11:40:17 -0400

    Finish Exercise 7: More Printing

---
+++ ex7.py
@@ -0,0 +1,28 @@
+# print a string
+print('Mary had a little lamb.')
+# print a formatted string
+print('Its fleece was white as {}'.format('snow'))
+# print a string
+print('And everywhere that Mary went.')
+# print a line consisting of periods
+print('.' * 10) # what'd that do?: 'multiplied' the string by 10 (concatenated ten occurences of the string)
+
+# set each character in the string "CheeseBurger" to a variable
+end1 = 'C'
+end2 = 'h'
+end3 = 'e'
+end4 = 'e'
+end5 = 's'
+end6 = 'e'
+end7 = 'B'
+end8 = 'u'
+end9 = 'r'
+end10 = 'g'
+end11 = 'e'
+end12 = 'r'
+
+# watch that comma at the end. try removing it to see what happens
+# print "Cheese " by using variables that contain each character
+print(end1 + end2 + end3 + end4 + end5 + end6, end=' ') # removing comma gives SyntaxError
+# print "Burger" by using variables that contain each character 
+print(end7 + end8 + end9 + end10 + end11 + end12)
