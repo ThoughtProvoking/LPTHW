5df1da49f7d32b87093d2a4498fc77a0ad0115f3
ThoughtProvoking <doreentlu0@gmail.com>
2017-06-04 13:08:13 -0400

    Finish Exercise 11: Asking Questions

---
+++ ex11.py
@@ -0,0 +1,31 @@
+# print a string
+print('How old are you?', end=' ')
+# store the user input
+age = input()
+# print a string
+print('How tall are you?', end=' ')
+# store the user input
+height = input()
+# print a strin
+print('How much do you weigh?', end=' ')
+# store the user input
+weight = input()
+# print a string and store the user input
+example = input("Give me something: ")
+
+# print the user inputs together
+print(f"So you're {age} old, {height} tall, and {weight} heavy.")
+# print the user input
+print(f'You gave me: {example}')
+
+# print a string and store the user input
+name = input("What is your name? ")
+# print a string
+print('How are you feeling?', end=' ')
+# store the user input
+mood = input()
+# print a string and store the user input
+color = input("What's your favorite color? ")
+
+# print the user inputs together
+print(f'Your name is {name}; you are feeling {mood} and you like the color {color}.')
