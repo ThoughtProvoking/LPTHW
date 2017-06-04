901b36d3bdc8da5831ff9aeb244dafc66fd1bf25
ThoughtProvoking <doreentlu0@gmail.com>
2017-06-04 13:16:00 -0400

    Finish Exercise 12: Prompting People

---
+++ ex12.py
@@ -0,0 +1,9 @@
+# print a string and store the user input
+age = input('How old are you? ')
+# print a string and store the user input
+height = input('How tall are you? ')
+# print a string and store the user input
+weight = input('How much do you weigh? ')
+
+# print the user inputs together
+print(f"So you're {age} old, {height} tall, and {weight} heavy.")
